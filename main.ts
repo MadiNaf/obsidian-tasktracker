2a589ca5ba4ab17b051f7d2bc1b1a7fd9d9b65cb
MadiBlh <madiats2@gmail.com>
2023-05-28 14:28:46 +0200

    progress-bar created

--- main.ts
+++ main.ts
@@ -3,10 +3,8 @@ import {FileSystemAdapter, MarkdownPostProcessorContext, Plugin, TFile, TFolder,
 /** ---------------------------------------------- \
  *              		MODELS                  
 \ ----------------------------------------------- */
-
-type TSize = 'Small' | 'Medium' | 'Large'
 interface TaskTrackerSetting {
-	size: TSize;
+	size: string;
 	colors: Array<string>;
 }
 
@@ -14,7 +12,7 @@ interface TaskTrackerConfig {
 	path: string;
 	fileName: string;
 	label?: string;
-	config?: TaskTrackerSetting
+	settings?: TaskTrackerSetting
 }
 
 interface TaskeLine {
@@ -35,9 +33,24 @@ class Taske {
 	}
 }
 
+/** ---------------------------------------------- \
+ *              		Constents                 
+\ ----------------------------------------------- */
+const RED = '#BF616A';
+const ORANGE = '#D08770';
+const YELLOW = '#EBCB8B';
+const GREEN = '#A3BE8C';
+
+const SIZES: {[key: string]: string} = {
+  sbar: '220px',
+  mbar: '245px',
+  lbar: '270px',
+  xlbar: '295px'
+}
+
 const DEFAULT_SETTINGS: TaskTrackerSetting = {
-	colors: ['default'],
-	size: 'Small'
+	colors: [RED, ORANGE, YELLOW, GREEN],
+	size: SIZES.sbar
 }
 
 /** ---------------------------------------------- \
@@ -69,8 +82,16 @@ export default class TaskTracker extends Plugin {
 
 			this.buildTaskTracker(el, config).then(() => {});
 		});
+
+		// this.app.workspace.on('active-leaf-change', (leaf: WorkspaceLeaf) => {});
 	}
 
+	/**
+	 * 
+	 * @param el 
+	 * @param config 
+	 * @returns 
+	 */
 	async buildTaskTracker(el: HTMLElement, config: TaskTrackerConfig): Promise<string> {
 		return new Promise(async (resolve, reject) => {
 			console.log('CONFIG ::: ', config);
@@ -87,6 +108,7 @@ export default class TaskTracker extends Plugin {
 					const fileContent = await this.app.vault.read(file);
 					const tasks = this.getFileTasks(fileContent);
 					console.log('TASKS ::: ', tasks);
+					this.crateProgressBar(el, config, tasks);
 				}
 	
 			} catch (error) {
@@ -113,6 +135,11 @@ export default class TaskTracker extends Plugin {
 		return targetFile as TFile;
 	}
 
+	/**
+	 * 
+	 * @param fileContent 
+	 * @returns 
+	 */
 	getFileTasks(fileContent: string): Taske {
 		const lines: string[] = fileContent.split('\n');
 		const taskList: TaskeLine[] = [];
@@ -136,11 +163,72 @@ export default class TaskTracker extends Plugin {
 		});
 
 		tasks = new Taske(todo, done, taskList);
-    console.log('Tasks:', tasks);
     return tasks;
 	}
 
-	crateProgressBar(): void {}
+	/**
+	 * 
+	 * @param el 
+	 * @param config 
+	 * @param tasks 
+	 */
+	crateProgressBar(el: HTMLElement, config: TaskTrackerConfig, tasks: Taske): void {
+		// Progression 
+		const { completedTasks, incompleteTask } = tasks;
+		const total: number = completedTasks + incompleteTask;
+		const taskProgression: number = Math.floor((completedTasks / total) * 100);
+
+		// Get user settings or use default
+		const { settings } = config;
+		const size = settings?.size ? settings.size : DEFAULT_SETTINGS.size;
+		const colors = settings?.colors ? settings.colors : DEFAULT_SETTINGS.colors;
+		const color: string = this.getProgressionColor(taskProgression, colors);
+
+		// Progress bar container HTML element
+		const progressbarContainer: HTMLElement = document.createElement('div');
+		progressbarContainer.classList.add('task-tracker-container');
+		progressbarContainer.style.width = SIZES?.[size];
+
+		// Progress bar HTML element
+		const progressBarElement: HTMLElement = document.createElement('div');
+		const progressionElement: HTMLElement = document.createElement('div');
+
+		progressBarElement.classList.add('task-tracker-progress-bar');
+		progressionElement.style.height = '100%';
+		progressionElement.style.width = `${taskProgression}%`;
+		progressionElement.style.backgroundColor = color;
+
+		// Progression text
+		const progressionText: HTMLElement = document.createElement('p');
+		const textNode: Text = document.createTextNode(`${taskProgression}%`);
+		progressionText.style.color = color;
+		progressionText.classList.add('task-progression-text');
+		progressionText.appendChild(textNode);
+
+		// Build HTML structure
+		progressBarElement.appendChild(progressionElement)
+		progressbarContainer.appendChild(progressBarElement)
+		progressbarContainer.appendChild(progressionText)
+		el.appendChild(progressbarContainer)
+	}
+
+	/**
+	 * 
+	 * @param progress 
+	 * @param colors 
+	 * @returns 
+	 */
+	getProgressionColor(progress: number, colors: string[]): string {
+		const from0To25: string = colors?.length ? colors [0] : GREEN;
+		const from26To50: string = colors?.length >= 2 ? colors[1] : from0To25;
+		const from51To99: string = colors?.length >= 3 ? colors[2] : from26To50;
+		const hundredPercent: string = colors?.length >= 4 ? colors[3] : from51To99;
+
+		if (progress <= 25) return from0To25;
+		else if (progress <= 50) return from26To50;
+		else if (progress <= 99) return from51To99;
+		else return hundredPercent;
+	}
 
 	/**
 	 * 
@@ -152,7 +240,6 @@ export default class TaskTracker extends Plugin {
 	}
 
 	onunload() {
-
 	}
 
 }
